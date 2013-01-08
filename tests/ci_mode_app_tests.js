01c76cf2cd8581354c0e3aca2e791ed9af69f7e7
Derek Brans <dbrans@gmail.com>
2013-01-07 17:33:17 -0800

    filling out tests for ci mode

--- tests/ci_mode_app_tests.js
+++ tests/ci_mode_app_tests.js
@@ -1,31 +1,111 @@
 var CiModeApp = require('../lib/ci_mode_app')
+  , Config = require('../lib/config')
+  , Server = require('../lib/server')
+  , test = require('./testutils.js')
+  , async = require('async')
+  , expect = test.expect
 
-describe('ci_mode_app', function() {
-  beforeEach(function() {
-    // Create a config with multiple mock launchers
-  })
+describe.only('ci_mode_app', function() {
 
-  it('should launch browsers in parallel', function() {
-    // Create a new app
-  })
+    // Setup
+    (function() {
+        var server_start = Server.prototype.start
 
-  it('should not stop when all runners finished but #runners < #launchers', function() {
+        before(function() {
+            // Short-circuit Server::start
+            Server.prototype.start = function() {
+                this.emit('server-start')
+            }
+        })
 
-  })
+        after(function() {
+            Server.prototype.start = server_start
+        })
+    }())
 
-  it('should not stop when #runners >= #launchers but not all runners finished ', function() {
 
-  })
+    // Helpers
+    var TIMEOUT = 0.5 // seconds
 
-  it('should stop when #runners >= #launchers AND all runners finished ', function() {
+    var tests = ['description']
 
-  })
+    function createLauncher(sleep, output) {
+        return {
+            command: "sleep " + (sleep/1000) +
+                     " && echo " + JSON.stringify(output),
+            type: "process"
+        }
+    }
 
-  it('stops after timeout, even if runners are not finished', function() {
+    function createApp(launchers) {
 
-  })
+        function createConfig(options) {
+            return {
+                get: function(key) {
+                    return options[key]
+                },
+                getLaunchers: function (app, cb) {
+                    cb(options.launchers)
+                }
+            }
+        }
 
-  it('writes test results to stdout when complete', function() {
+        var launch_in_ci = []
+        for(var name in launchers) {
+            launch_in_ci.push(name)
+        }
 
-  })
-});
+        var app = new CiModeApp(createConfig({
+            timeout: TIMEOUT,
+            launchers: launchers,
+            launch_in_ci: launch_in_ci
+        }))
+
+        // Disable app.quit
+        app.quit = function() {}
+
+        return app
+    }
+
+
+    function createLaunchers() {
+        return {
+            launcher1: createLauncher(10, '1..2\nok 1 - A\nok 2 - B\n'),
+            launcher2: createLauncher(10, '1..2\nok 1 - A\nnot ok 2 - B\n')
+        }
+    }
+
+    it('should launch in parallel', function() {
+        var launchers = createLaunchers()
+        var app = createApp(launchers)
+        for(var name in launchers) {
+            expect(launchers[name].runner).to.be.ok
+        }
+    })
+
+
+    // Exit Conditions
+    it('should not stop when all runners finished but #runners < #launchers', function() {
+        expect(true).to.be.ok
+        expect(false).to.be.ok
+    })
+
+    it('should not stop when #runners >= #launchers but not all runners finished ', function() {
+
+    })
+
+    it('should stop when #runners >= #launchers AND all runners finished ', function() {
+
+    })
+
+    it('stops after timeout, even if runners are not finished', function() {
+
+    })
+
+
+    // Results
+    it('writes test results to stdout when complete', function() {
+
+    })
+
+})
